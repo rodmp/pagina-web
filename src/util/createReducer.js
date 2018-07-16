3018be6fc5c4ec9cb939d179c8a368d0f3be515f
rodmp <rodomiranda2020@gmail.com>
2018-07-15 20:15:26 -0400

    Create store updates

--- src/util/createReducer.js
+++ src/util/createReducer.js
@@ -1,10 +1,18 @@
+import { has } from 'ramda'
 import Result from 'folktale/result'
+
 import reportError from 'sls-aws/src/util/reportError'
 
 const createReducer = (reducerObj, initialState) => (
 	state = initialState, action
 ) => Result.try(
-	() => reducerObj[action.type](state, action.payload)
+	() => {
+		const { type } = action
+		if (!has(type, reducerObj)) {
+			throw new Error(`Action ${type} not found`)
+		}
+		reducerObj[action.type](state, action.payload)
+	}
 ).mapError(reportError).getOrElse(state)
 
 export default createReducer
