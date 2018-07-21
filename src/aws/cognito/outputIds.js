0a1979b1bc6fa41020bb038e06330cf597264ca3
billjohnston <billjohnston4@gmail.com>
2018-07-21 18:36:44 -0400

    Outputs

---
+++ src/aws/cognito/outputIds.js
@@ -0,0 +1,5 @@
+import resourcePrefix from 'sls-aws/src/aws/util/resourcePrefix'
+
+export const IDENTITY_POOL_ID = `${resourcePrefix}IdentityPoolId`
+export const CLIENT_ID = `${resourcePrefix}ClientId`
+export const USER_POOL_ID = `${resourcePrefix}UserPoolId`
