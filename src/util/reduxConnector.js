522a3ade047da7fac83b97e5e46ed04b1b19ac90
rodmp <rodomiranda2020@gmail.com>
2018-08-04 15:51:20 -0400

    reduxConnector

---
+++ src/util/reduxConnector.js
@@ -0,0 +1,15 @@
+import { reduce, assoc } from 'ramda'
+import { connect } from 'react-redux'
+import { withStyles } from '@material-ui/core/styles'
+
+export default (component, selectors = [], actions = [], styles) => {
+	const styledComponent = styles ? withStyles(styles, component) : component
+	return connect(
+		(state, props) => reduce((results, [functionName, fn]) => assoc(
+			functionName, fn(state, props), results
+		), {}, selectors),
+		reduce((results, [functionName, fn]) => assoc(
+			functionName, fn, results
+		), {}, actions)
+	)(styledComponent)
+}
