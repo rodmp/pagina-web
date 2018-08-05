57e2f69531eb5fb1aff6812985b7bd0e4a489e0d
billjohnston <billjohnston4@gmail.com>
2018-08-05 11:20:11 -0400

    Forgot to export

--- src/constants/connectors/routeRenderConnector.js
+++ src/constants/connectors/routeRenderConnector.js
@@ -2,6 +2,6 @@ import reduxConnector from 'sls-aws/src/util/reduxConnector'
 
 import currentRouteModuleTypes from 'sls-aws/src/client-logic/route/selectors/currentRouteModuleTypes'
 
-reduxConnector(
+export default reduxConnector(
 	[['currentRouteModuleTypes', currentRouteModuleTypes]]
 )
