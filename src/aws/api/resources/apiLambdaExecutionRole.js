252310285aed4cf9e4557096e24f4b9e5b1b171a
billjohnston <billjohnston4@gmail.com>
2018-08-25 09:28:33 -0400

    Lambda fn partial

---
+++ src/aws/api/resources/apiLambdaExecutionRole.js
@@ -0,0 +1,38 @@
+import {
+	API_LAMBDA_EXECUTION_ROLE, IDENTITY_POOL,
+} from 'sls-aws/src/aws/cognito/resourceIds'
+
+export default {
+	[API_LAMBDA_EXECUTION_ROLE]: {
+		Type: 'AWS::IAM::Role',
+		Properties: {
+			AssumeRolePolicyDocument: {
+				Version: '2012-10-17',
+				Statement: [
+					{
+						Effect: 'Allow',
+						Principal: {
+							Service: ['lambda.amazonaws.com'],
+						},
+						Action: ['sts:AssumeRole'],
+					},
+				],
+			},
+			Policies: [
+				// {
+				// 	PolicyName: 'root',
+				// 	PolicyDocument: {
+				// 		Version: '2012-10-17',
+				// 		Statement: [
+				// 			{
+				// 				Effect: 'Allow',
+				// 				Action: ['logs:*'],
+				// 				Resource: 'arn:aws:logs:*:*:*',
+				// 			},
+				// 		],
+				// 	},
+				// },
+			],
+		},
+	},
+}
---
+++ src/aws/util/fnBuildPath.js
@@ -0,0 +1,1 @@
+export default fnName => `./build-lambda/${fnName}.js`
