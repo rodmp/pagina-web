ffe86ab50bc779187be91d096aa0808c80071870
billjohnston <billjohnston4@gmail.com>
2018-09-15 12:36:37 -0400

    Payload schema errors

--- src/server/api/getApiPromise.js
+++ src/server/api/getApiPromise.js
@@ -1,9 +1,10 @@
 import { prop, propOr } from 'ramda'
 
+import ajvErrors from 'sls-aws/src/util/ajvErrors'
 import validateSchema from 'sls-aws/src/util/validateSchema'
 import { actionProp } from 'sls-aws/src/server/api/lenses'
 
-const validateOrNah = (schemaType, endpointId, endpointDesc, payload) => {
+const validateOrNah = (schemaType, endpointId, endpointDesc) => (payload) => {
 	const schema = prop(schemaType, endpointDesc)
 	if (schema) {
 		return validateSchema(endpointId, schema, payload).then((res) => {
@@ -12,7 +13,7 @@ const validateOrNah = (schemaType, endpointId, endpointDesc, payload) => {
 			}
 			return Promise.reject({
 				statusCode: 400,
-				schemaErrors: prop('errors', res),
+				schemaErrors: ajvErrors(schema, prop('errors', res)),
 			})
 		})
 	}
@@ -20,7 +21,7 @@ const validateOrNah = (schemaType, endpointId, endpointDesc, payload) => {
 }
 
 
-const mutatePayloadOrNah = (mutateType, endpointDesc, payload) => {
+const mutatePayloadOrNah = (mutateType, endpointDesc) => (payload) => {
 	const mutatePromise = prop(mutateType, endpointDesc)
 	return mutatePromise ? mutatePromise(payload) : Promise.resolve(payload)
 }
@@ -39,10 +40,10 @@ export default (endpoints, endpointId, payload) => {
 	}
 	return [
 		propOr(Promise.resolve(payload), 'authentication'),
-		validateOrNah('payloadSchema', endpointId, endpointDesc, payload),
-		mutatePayloadOrNah('toInternalValue', endpointDesc, payload),
+		validateOrNah('payloadSchema', endpointId, endpointDesc),
+		mutatePayloadOrNah('toInternalValue', endpointDesc),
 		actionPromise,
-		mutatePayloadOrNah('toRepresentation', endpointDesc, payload),
-		validateOrNah('responseSchema', endpointId, endpointDesc, payload),
+		mutatePayloadOrNah('toRepresentation', endpointDesc),
+		validateOrNah('responseSchema', endpointId, endpointDesc),
 	]
 }
