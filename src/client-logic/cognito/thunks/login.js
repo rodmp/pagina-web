d0196ce8e208bf48f36ee82c848379acc4d91971
billjohnston <billjohnston4@gmail.com>
2018-09-16 12:04:57 -0400

    Fix some initial auth stuff

--- src/client-logic/cognito/thunks/login.js
+++ src/client-logic/cognito/thunks/login.js
@@ -3,6 +3,7 @@ import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js'
 import userPool from 'sls-aws/src/client-logic/cognito/util/userPool'
 
 import pushRoute from 'sls-aws/src/client-logic/route/thunks/pushRoute'
+import determineAuth from 'sls-aws/src/client-logic/cognito/thunks/determineAuth'
 import { DASHBOARD_ROUTE_ID } from 'sls-aws/src/descriptions/routes/routeIds'
 
 export default ({ email, password }) => dispatch => new Promise(
@@ -13,15 +14,17 @@ export default ({ email, password }) => dispatch => new Promise(
 		})
 		const cognitoUser = new CognitoUser({
 			Username: email,
-			Pool: userPool
+			Pool: userPool,
 		})
 		cognitoUser.authenticateUser(authenticationDetails, {
 			onSuccess: resolve,
 			onFailure: reject,
 		})
-	}
+	},
 ).then(
-	() => dispatch(pushRoute(DASHBOARD_ROUTE_ID))
+	() => dispatch(determineAuth()).then(
+		() => dispatch(pushRoute(DASHBOARD_ROUTE_ID)),
+	),
 ).catch(
-	console.warn
+	console.warn,
 )
--- src/client-logic/route/util/defaultRoute.js
+++ src/client-logic/route/util/defaultRoute.js
@@ -1,10 +1,10 @@
 import isAuthenticated from 'sls-aws/src/client-logic/auth/selectors/isAuthenticated'
 import {
-	DASHBOARD_ROUTE_ID, LOGIN_ROUTE_ID, SIGN_UP_ROUTE_ID
+	DASHBOARD_ROUTE_ID, LOGIN_ROUTE_ID, SIGN_UP_ROUTE_ID,
 } from 'sls-aws/src/descriptions/routes/routeIds'
 
-export default () => (
-	isAuthenticated() ?
+export default state => (
+	isAuthenticated(state) ?
 		{ routeId: DASHBOARD_ROUTE_ID, routeParams: {} } :
 		{ routeId: SIGN_UP_ROUTE_ID, routeParams: {} }
 		// { routeId: LOGIN_ROUTE_ID, routeParams: {} }
