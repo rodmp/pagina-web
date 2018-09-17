7d6ae031bbb77aef6f1b2ab94c1491abeb7e79dd
billjohnston <billjohnston4@gmail.com>
2018-09-16 22:05:00 -0400

    Remove some constants

--- src/client-logic/cognito/thunks/signUp.js
+++ src/client-logic/cognito/thunks/signUp.js
@@ -1,15 +1,9 @@
-// import { CognitoUserAttribute } from 'amazon-cognito-identity-js'
-
 import userPool from 'sls-aws/src/client-logic/cognito/util/userPool'
 import pushRoute from 'sls-aws/src/client-logic/route/thunks/pushRoute'
 import { VERIFY_ACCOUNT_ROUTE_ID } from 'sls-aws/src/descriptions/routes/routeIds'
 
 export default ({ email, password }) => dispatch => new Promise(
 	(resolve, reject) => {
-		// const attributeEmail = new CognitoUserAttribute({
-		// 	Name: 'email',
-		// 	Value: email,
-		// })
 		userPool.signUp(
 			email, password, [], null, (cognitoError, result) => {
 				if (cognitoError) {
