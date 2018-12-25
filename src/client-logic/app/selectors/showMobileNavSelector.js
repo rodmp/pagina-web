781dcb77933e0ca42abfb46781dbe745b5a32b2f
rodmp <rodomiranda2020@gmail.com>
2018-12-24 22:46:47 -0500

    navigationConnector

---
+++ src/client-logic/app/selectors/showMobileNavSelector.js
@@ -0,0 +1,10 @@
+import { not, compose } from 'ramda'
+
+import { appStoreLenses } from 'sls-aws/src/client-logic/app/lenses'
+
+const { viewGtSm } = appStoreLenses
+
+export default compose(
+	not,
+	viewGtSm,
+)
