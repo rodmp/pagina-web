bfee425bb07a70b3c13d884a021848c6c4fdf8af
Bill <billjohnston4@gmail.com>
2018-12-31 15:08:42 -0500

    Autocomplete partial

---
+++ src/client-logic/form/selectors/multiFieldValue.js
@@ -0,0 +1,10 @@
+import { compose, pathOr } from 'ramda'
+
+import { formStoreLenses } from 'sls-aws/src/client-logic/form/lenses'
+
+const { pathOrFormInputs } = formStoreLenses
+
+export default (state, { moduleKey, fieldPath }) => compose(
+	pathOr([], fieldPath),
+	pathOrFormInputs(moduleKey, {}),
+)(state)
--- src/server/api/twitchApi.js
+++ src/server/api/twitchApi.js
@@ -1,11 +1,23 @@
 import nodeAjax from 'sls-aws/src/util/nodeAjax'
 
 const clientId = 'bywvja4ootazhbae0qcpwpsnu8glaa'
-const baseUrl = 'https://api.twitch.tv/helix/'
+const baseUrlNewApi = 'https://api.twitch.tv/helix/'
+const baseUrlV5 = 'https://api.twitch.tv/kraken/'
 
-// eslint-disable-next-line
 export const getUserData = loginArray => nodeAjax({
-	url: `${baseUrl}users`,
+	url: `${baseUrlNewApi}users`,
 	headers: { 'Client-ID': clientId },
 	queryParams: { login: loginArray },
+})
+
+export const getGame = gameId => nodeAjax({
+	url: `${baseUrlNewApi}search/games`,
+	headers: { 'Client-ID': clientId },
+	queryParams: { id: gameId },
+})
+
+export const searchGames = searchTerm => nodeAjax({
+	url: `${baseUrlV5}games`,
+	headers: { 'Client-ID': clientId },
+	queryParams: { query: searchTerm },
 })
