a42651a07a21b16e87606075051260ce7bc202b9
rodmp <rodomiranda2020@gmail.com>
2019-01-05 16:55:34 -0500

    Project card partial

--- src/client-logic/api/connectors/listModuleConnector.js
+++ src/client-logic/api/connectors/listModuleConnector.js
@@ -1,10 +1,12 @@
 import reduxConnector from 'sls-aws/src/util/reduxConnector'
 
 import getListSelector from 'sls-aws/src/client-logic/api/selectors/getListSelector'
+import listTypeSelector from 'sls-aws/src/client-logic/list/selectors/listTypeSelector'
 
 export default reduxConnector(
 	[
 		['list', getListSelector],
+		['listType', listTypeSelector],
 	],
 	// [
 	// 	['addSubForm', addSubForm],
--- src/client-web/list/ListModule.js
+++ src/client-web/list/ListModule.js
@@ -1,20 +1,34 @@
 import { map } from 'ramda'
 import React, { memo } from 'react'
 
-import ProjectListItem from 'sls-aws/src/client-web/list/ProjectListItem'
+import { ternary } from 'sls-aws/src/util/ramdaPlus'
 
+import ProjectCard from 'sls-aws/src/client-web/list/ProjectCard'
+import MaxWidthContainer from 'sls-aws/src/client-web/base/MaxWidthContainer'
 import withModuleContext from 'sls-aws/src/util/withModuleContext'
 
 import List from '@material-ui/core/List'
 
 import listModuleConnector from 'sls-aws/src/client-logic/api/connectors/listModuleConnector'
 
-export const ListModule = memo(({ list }) => (
+export const ListModule = memo(({ list, listType }) => ternary(
+	listType === 'card',
+	<div className="flex layout-row layout-align-center-start">
+		<MaxWidthContainer>
+			<div className="flex layout-row layout-align-center">
+				<div className="layout-row layout-wrap">
+					{map(recordId => (
+						<ProjectCard key={recordId} recordId={recordId} />
+					), list)}
+				</div>
+			</div>
+		</MaxWidthContainer>
+	</div>,
 	<List>
 		{map(recordId => (
-			<ProjectListItem key={recordId} recordId={recordId} />
+			<ProjectCard key={recordId} recordId={recordId} />
 		), list)}
-	</List>
+	</List>,
 ))
 
 export default withModuleContext(listModuleConnector(ListModule))
