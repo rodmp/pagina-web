808317bd6f154151e2ad6127177146688966686e
rodmp <rodomiranda2020@gmail.com>
2019-01-17 09:59:08 -0500

    Share menu

---
+++ src/client/web/base/ShareMenu.js
@@ -0,0 +1,73 @@
+import React, { memo, createElement } from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+import {
+	TwitterShareButton, FacebookShareButton, RedditShareButton, VKShareButton,
+	TwitterIcon, FacebookIcon, RedditIcon, VKIcon,
+} from 'react-share'
+
+const ShareMenuItems = ({ url }) => [
+	<MenuItem key="twitter">
+		<TwitterShareButton url={url}>
+			<div className="layout-row layout-align-start-center">
+				<TwitterIcon size={32} round />{'\u00A0'}<span>Share on Twitter</span>
+			</div>
+		</TwitterShareButton>
+	</MenuItem>,
+	<MenuItem key="facebook">
+		<FacebookShareButton url={url}>
+			<div className="layout-row layout-align-start-center">
+				<FacebookIcon size={32} round />{'\u00A0'}<span>Share on Facebook</span>
+			</div>
+		</FacebookShareButton>
+	</MenuItem>,
+	<MenuItem key="reddit">
+		<RedditShareButton url={url}>
+			<div className="layout-row layout-align-start-center">
+				<RedditIcon size={32} round />{'\u00A0'}<span>Share on Reddit</span>
+			</div>
+		</RedditShareButton>
+	</MenuItem>,
+	<MenuItem key="vk">
+		<VKShareButton url={url}>
+			<div className="layout-row layout-align-start-center">
+				<VKIcon size={32} round />{'\u00A0'}<span>Share on VK</span>
+			</div>
+		</VKShareButton>
+	</MenuItem>,
+]
+
+export default memo(() => {
+	const [anchorEl, setAnchorEl] = React.useState(null)
+	const open = Boolean(anchorEl)
+
+	function handleClick(event) {
+		setAnchorEl(event.currentTarget)
+	}
+
+	function handleClose() {
+		setAnchorEl(null)
+	}
+
+	return (
+		<div>
+			<IconButton
+				aria-label="More"
+				aria-haspopup="true"
+				onClick={handleClick}
+			>
+				<MoreVertIcon />
+			</IconButton>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+			>
+				<ShareMenuItems url="http://google.com" />
+			</Menu>
+		</div>
+	)
+})
