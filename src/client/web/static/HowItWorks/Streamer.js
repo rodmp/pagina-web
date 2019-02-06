db9c66c17ecd7fe1c21ae246ba393e907b402d91
jbechtold <joebechtold@gmail.com>
2019-02-06 15:41:24 -0500

    copy updates for how it works

--- src/client/web/static/HowItWorks/Streamer.js
+++ src/client/web/static/HowItWorks/Streamer.js
@@ -3,7 +3,6 @@ import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 import Card from 'sls-aws/src/client/web/base/Card'
 import StarIcon from 'sls-aws/src/client/assets/star-regular.svg'
-import NotesIcon from 'sls-aws/src/client/assets/notes-medical-solid.svg'
 import FlagIcon from 'sls-aws/src/client/assets/flag-regular.svg'
 import TvIcon from 'sls-aws/src/client/assets/tv-solid.svg'
 import SmileIcon from 'sls-aws/src/client/assets/smile-regular.svg'
@@ -11,23 +10,21 @@ import styles from './style'
 
 const Streamer = ({ classes }) => (
 	<div className={classNames('flex', 'layout-row', 'layout-wrap', 'layout-xs-column', 'layout-sm-column', 'layout-md-column', classes.cardWrapper)}>
-		<Card title="Claim" icon={StarIcon}>
-			<p>Go to the challenge &amp; accept the dare.</p>
-		</Card>
-		<Card title="Verify" icon={NotesIcon}>
-			<p>You’ll have to verify that you are who you say you are.</p>
+		<Card title="Accept" icon={StarIcon}>
+			<p>Verify your Twitch Channel from the pledge page to accept a Dare.</p>
 		</Card>
 		<Card title="Goal" icon={FlagIcon}>
-			<p>Set a goal amount for the pot, or just deliver.</p>
+			<p>Set a goal for the Bounty. Remember that only 90% can usually be 
+				collected due to transaction issues, so take that into account. 
+				<strong>Then mention the Dare on your stream!</strong></p>
 		</Card>
 		<Card title="Stream" icon={TvIcon}>
-			<p>
-        When your goal is reached, live stream & upload the video of you meeting the challenge
-        requirements.
+			<p>When the Bounty reaches your goal we will let you know. Then, 
+				live stream and upload video proof to Double Dog within 30 days.
 			</p>
 		</Card>
 		<Card title="Reward" icon={SmileIcon}>
-			<p>We’ll verify that you delivered &amp; you’ll get paid.</p>
+			<p>We will verify the proof and you’ll get paid!</p>
 		</Card>
 	</div>
 )
