8b04fada2dc3c949e1a8c0a898a0a54aee5d4add
AnnaBova <anna.bova23@gmail.com>
2019-02-21 11:13:43 +0200

    Change banner how-it-works/my-dares

--- src/shared/descriptions/modules/howItWorkBannerHeader.js
+++ src/shared/descriptions/modules/howItWorkBannerHeader.js
@@ -1,12 +1,14 @@
 import { HOW_IT_WORK_BANNER_HEADER_MODULE_ID } from 'sls-aws/src/shared/descriptions/modules/moduleIds'
 
-import howItWorks from 'sls-aws/src/client/assets/howItWorks.png'
+import howItWorks from 'sls-aws/src/client/assets/How-it-works.png'
 
 export default {
 	[HOW_IT_WORK_BANNER_HEADER_MODULE_ID]: {
 		moduleType: 'bannerHeader',
 		bannerImage: howItWorks,
-		bannerImageText: 'How It Works',
+		bannerImageText: 'HOW IT WORKS',
+		bannerImageSubText: 'Dare a streamer, pledge toward a Bounty, and watch the proof!',
 		bannerSubText: 'Who are you?',
+		textWithBg: true,
 	},
 }
