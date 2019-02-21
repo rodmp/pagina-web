9e620712ca94ffab7021e023a05aeacaf17042c9
AnnaBova <anna.bova23@gmail.com>
2019-02-21 11:13:43 +0200

    Change banner how-it-works/my-dares

--- src/shared/descriptions/modules/myProjectsBannerHeader.js
+++ src/shared/descriptions/modules/myProjectsBannerHeader.js
@@ -6,13 +6,14 @@ import {
 	CREATE_PROJECT_ROUTE_ID,
 } from 'root/src/shared/descriptions/routes/routeIds'
 
-import myprojects from 'root/src/client/assets/myprojects.jpg'
+import myDare from 'root/src/client/assets/Your-Dare.jpg'
 
 export default {
 	[MY_PROJECT_BANNER_HEADER_MODULE_ID]: {
 		moduleType: 'bannerHeader',
-		bannerImage: myprojects,
-		bannerImageText: 'YOUR DARES',
+		bannerImage: myDare,
+		bannerImageText: 'DARE A STREAMER',
+		bannerImageSubText: 'Pledge to a Dare you like, or create your own!',
 		textWithBg: true,
 		bannerSubText: 'Select a Dare',
 		link: {
