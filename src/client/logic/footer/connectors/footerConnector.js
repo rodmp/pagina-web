411d49e1dcfad4fb59a59b55137b5de827f81b48
piekarskid <42898366+piekarskid@users.noreply.github.com>
2019-02-22 02:17:24 +0100

    added selector for footer icons (#36)

--- src/client/logic/footer/connectors/footerConnector.js
+++ src/client/logic/footer/connectors/footerConnector.js
@@ -1,9 +1,11 @@
 import reduxConnector from 'sls-aws/src/shared/util/reduxConnector'
 
 import bannerFooterImageSelector from 'sls-aws/src/client/logic/footer/selectors/bannerFooterImageSelector'
+import isSuccessPageSelector from 'sls-aws/src/client/logic/footer/selectors/isSuccessPageSelector'
 
 export default reduxConnector(
 	[
 		['bannerFooterImage', bannerFooterImageSelector],
+		['isSuccessPage', isSuccessPageSelector],
 	],
 )
