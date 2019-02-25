80565e7580a114d386e27d28fa219f53577079ab
rodmp <rodomiranda2020@gmail.com>
2019-02-24 20:13:18 -0500

    Fix hostedZoneId

--- src/aws/util/domainName.js
+++ src/aws/util/domainName.js
@@ -2,6 +2,6 @@ import { stage } from 'root/src/aws/util/resourcePrefix'
 
 export const apexDomain = 'watt.tv'
 
-export const hostedZoneId = 'ZLGAJQMR826S3'
+export const hostedZoneId = 'Z1Y1YRSE1A6N3N'
 
 export default `${stage.toLowerCase()}.${apexDomain}`
