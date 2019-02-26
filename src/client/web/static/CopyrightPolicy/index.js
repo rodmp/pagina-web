5d1076c959b66f5c2638fe2ccb511025c5415009
jbechtold <joebechtold@gmail.com>
2019-02-26 18:42:06 -0500

    Copyright Policy still broken

--- src/client/web/static/CopyrightPolicy/index.js
+++ src/client/web/static/CopyrightPolicy/index.js
@@ -7,23 +7,15 @@ import pageContent from './pageContent'
 
 const CopyrightPolicy = ({ classes }) => (
 	<Terms>
-		<section>
-		It is our policy to respond to clear notices of alleged copyright 
-		infringement in accordance with 
-		the <link href="https://www.copyright.gov/legislation/dmca.pdf">
-		Digital Millennium Copyright Act of 1998.</link> Double Dog removes 
-		material that is the subject of a compliant DMCA takedown notice. 
-		Section 512 of the DMCA provides the rules for reporting copyright 
-		infringement and for filing a counter-notification.
-		</section>
-		<React.Fragment>
-			{map(({ title, text }) => (
-				<section className={classes.section}>
-					{title ? <h3 className={classes.sectionTitle}>{title}</h3> : null}
+		{map(
+			({ title, text }) => (
+				<section className={classes.section} key={title}>
+					<h3 className={classes.sectionTitle}>{title}</h3>
 					{text}
 				</section>
-			), pageContent(classes))}
-		</React.Fragment>
+			),
+			pageContent(classes),
+		)}
 	</Terms>
 )
 
