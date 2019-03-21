1d0d2cd5b6e618e6fb9e5f9b1b5b3b972cc8e313
rodmp <rodomiranda2020@gmail.com>
2019-03-21 23:22:32 +0100

    made handler pure function

--- src/client/logic/form/handlers/autoCompleteSetInputHandler.js
+++ src/client/logic/form/handlers/autoCompleteSetInputHandler.js
@@ -1,7 +1,7 @@
 import { length, gt } from 'ramda'
 
 export default (moduleKey, fieldPath, maxItems, action) => (value) => {
-	const validateMaxValue = value
+	const validateMaxValue = [...value]
 	action(moduleKey, fieldPath, validateMaxValue)
 	if (gt(length(value), maxItems)) {
 		validateMaxValue.pop()
