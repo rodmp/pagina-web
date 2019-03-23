894cd4f523af608e9965a6948e53765b316df213
Salvatore Randazzo <randazzo.salvatore86@gmail.com>
2019-03-23 14:11:36 -0400

    Added client logic

--- src/client/logic/project/selectors/canPledgeProjectSelector.js
+++ src/client/logic/project/selectors/canPledgeProjectSelector.js
@@ -1,12 +1,13 @@
 import { equals, isNil } from 'ramda'
 
-import { projectApprovedKey } from 'root/src/server/api/lenses'
+import { projectApprovedKey, projectRejectedKey } from 'root/src/server/api/lenses'
 import statusSelector from 'root/src/client/logic/project/selectors/statusSelector'
 import isAuthenticated from 'root/src/client/logic/auth/selectors/isAuthenticated'
 import myPledgeSelector from 'root/src/client/logic/project/selectors/myPledgeSelector'
 
 export default (state, props) => (
 	equals(statusSelector(state, props), projectApprovedKey) &&
+	equals(statusSelector(state, props), projectRejectedKey) &&
 	isAuthenticated(state, props) &&
 	isNil(myPledgeSelector(state, props))
 )
