6edfbdcd8d112daeb63251ba7484590f2de1a55c
New System <user@macbook-pro-new.mynet>
2019-03-26 20:02:58 +0100

    cleared feedback on textFieldSetInputHandler

--- src/client/logic/form/handlers/textFieldSetInputHandler.js
+++ src/client/logic/form/handlers/textFieldSetInputHandler.js
@@ -5,6 +5,11 @@ import {
 	zipCodeValidation,
 } from 'root/src/client/logic/form/util/creditCardValidations'
 
+const cardNumber = 'cardNumber'
+const securityCode = 'securityCode'
+const expDate = 'expDate'
+const zipCode = 'zipCode'
+
 export default (moduleKey, fieldPath, action, fieldType, setPreviousValue, previousValue) => async (e) => {
 	e.preventDefault()
 	let { value } = e.target
@@ -17,17 +22,20 @@ export default (moduleKey, fieldPath, action, fieldType, setPreviousValue, previ
 		value = parseInt(value, 10)
 		if (value > 999999) value = 999999
 	}
-	if (fieldPath[0] === 'cardNumber') {
-		value = cardNumberValidation(value)
-	}
-	if (fieldPath[0] === 'securityCode') {
-		value = securityCodeValidation(value)
-	}
-	if (fieldPath[0] === 'expDate') {
-		value = expirationDateValidation(value, previousValue)
-	}
-	if (fieldPath[0] === 'zipCode') {
-		value = zipCodeValidation(value)
+	switch (fieldPath[0]) {
+		case cardNumber:
+			value = cardNumberValidation(value)
+			break
+		case securityCode:
+			value = securityCodeValidation(value)
+			break
+		case expDate:
+			value = expirationDateValidation(value, previousValue)
+			break
+		case zipCode:
+			value = zipCodeValidation(value)
+			break
+		default:
 	}
 	action(moduleKey, fieldPath, value)
 	await setPreviousValue(value.slice(0, value.length))
