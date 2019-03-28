5a73c2cd8c550802825073dccd6f4d55f8ee29f7
rodmp <rodomiranda2020@gmail.com>
2019-03-28 21:04:24 +0100

    added all possibilities for special character matching in regex

--- src/shared/descriptions/endpoints/schemas/changePasswordPayloadSchema.js
+++ src/shared/descriptions/endpoints/schemas/changePasswordPayloadSchema.js
@@ -7,7 +7,7 @@ export default {
 		newPassword: {
 			type: 'string',
 			minLength: 6,
-			pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\[\~\`\[#$^+\\\\=\\\]!*\_()@%&\{\}\?\/\.\>\,\<\;\:\'\"\|-]).{6,}$',
+			pattern: '^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[ \[\~\`\[#$^+\\\\=\\\]!*\_()@%&\{\}\?\/\.\>\,\<\;\:\'\"\|-]).{6,}$',
 			errorMessage: {
 				minLength: 'New password is too short',
 				pattern: 'New password must contain at least one uppercase letter, one lowercase letter, one number and one special character',
