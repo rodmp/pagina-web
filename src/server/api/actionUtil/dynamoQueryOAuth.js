86d1a8a42a39f9d46b682663e68d721e52c7dabc
rodmp <rodomiranda2020@gmail.com>
2019-03-30 13:23:53 +0100

    fixed serialization on backend

--- src/server/api/actionUtil/dynamoQueryOAuth.js
+++ src/server/api/actionUtil/dynamoQueryOAuth.js
@@ -1,13 +1,14 @@
 import { TABLE_NAME, documentClient } from 'root/src/server/api/dynamoClient'
-import { PARTITION_KEY } from 'root/src/shared/constants/apiDynamoIndexes'
+import { PARTITION_KEY, SORT_KEY } from 'root/src/shared/constants/apiDynamoIndexes'
 import { dynamoItemsProp } from 'root/src/server/api/lenses'
 
 export default (async (userId) => {
 	const oAuthParams = {
 		TableName: TABLE_NAME,
-		KeyConditionExpression: `${PARTITION_KEY} = :pk`,
+		KeyConditionExpression: `${PARTITION_KEY} = :pk and begins_with(${SORT_KEY}, :token)`,
 		ExpressionAttributeValues: {
 			':pk': `user-${userId}`,
+			':token': 'token',
 		},
 		ConsistentRead: true,
 	}
