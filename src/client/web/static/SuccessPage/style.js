5b830fde2d77718f1fd9aaafacdc19f0f3013463
rodmp <rodomiranda2020@gmail.com>
2019-04-01 09:32:09 -0400

    added background position to smaller sizes

--- src/client/web/static/SuccessPage/style.js
+++ src/client/web/static/SuccessPage/style.js
@@ -59,17 +59,19 @@ const styles = {
 		minHeight: '150px !important',
 		'@media (max-width: 636px)': {
 			minHeight: '200px !important',
+			backgroundPositionY: '33% !important',
 		},
 		'@media (min-width: 636px)': {
 			minHeight: '300px !important',
+			backgroundPositionY: '33% !important',
 		},
 		'@media (min-width: 1280px)': {
 			minHeight: '399px !important',
-			backgroundPositionY: '33%',
+			backgroundPositionY: '33% !important',
 		},
 		'@media (min-width: 1600px)': {
 			minHeight: '600px !important',
-			backgroundPositionY: '33%',
+			backgroundPositionY: '33% !important',
 		},
 	},
 	icons: {
