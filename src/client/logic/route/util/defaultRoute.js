fd19cbb64f5a55971e36e095d2d3bcdbb9c84476
AnnaBova <anna.bova23@gmail.com>
2019-04-03 09:06:07 +0300

    change default route

--- src/client/logic/route/util/defaultRoute.js
+++ src/client/logic/route/util/defaultRoute.js
@@ -1,10 +1,5 @@
-import isAuthenticated from 'root/src/client/logic/auth/selectors/isAuthenticated'
 import {
-	ACTIVE_PROJECTS_ROUTE_ID, LOGIN_ROUTE_ID,
+	ACTIVE_PROJECTS_ROUTE_ID,
 } from 'root/src/shared/descriptions/routes/routeIds'
 
-export default state => (
-	isAuthenticated(state)
-		? { routeId: ACTIVE_PROJECTS_ROUTE_ID, routeParams: {} }
-		: { routeId: LOGIN_ROUTE_ID, routeParams: {} }
-)
+export default () => ({ routeId: ACTIVE_PROJECTS_ROUTE_ID, routeParams: {} })
