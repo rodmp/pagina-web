c017e9277f8805877d5c4a9fa7606cf115126edf
piekarskid <42898366+piekarskid@users.noreply.github.com>
2019-04-03 16:50:12 +0200

    Update SubTitle.js

--- src/client/web/typography/SubTitle.js
+++ src/client/web/typography/SubTitle.js
@@ -1,22 +1,24 @@
+import React, { memo } from 'react'
+
 import { fontFamily } from 'root/src/client/web/commonStyles'
 import { withStyles } from '@material-ui/core/styles'
 
 import classNames from 'classnames'
 
 const styles = {
-  fontStyle: {
-    fontFamily,
-    martginTop: 14,
-    fontSize: 24,
-    lineHeight: 1.25,
-    maxWidth: 405
-  },
+	fontStyle: {
+		fontFamily,
+		martginTop: 14,
+		fontSize: 24,
+		lineHeight: 1.25,
+		maxWidth: 405,
+	},
 }
 
 export const TitleUnstyled = memo((({ classes, children, additionalClass }) => (
 	<div className={classNames(classes.fontStyle, additionalClass)}>
-    {children}
-  </div>
+		{children}
+	</div>
 )))
 
 export default withStyles(styles)(TitleUnstyled)
