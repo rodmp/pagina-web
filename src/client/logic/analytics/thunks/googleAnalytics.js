8865a30e1d055306b1a2c2e098aa9e112f6c4a9a
rodmp <rodomiranda2020@gmail.com>
2019-04-04 22:17:26 +0200

    set up base

--- src/client/logic/analytics/thunks/googleAnalytics.js
+++ src/client/logic/analytics/thunks/googleAnalytics.js
@@ -1,9 +1,10 @@
 import ReactGA from 'react-ga'
 import { googleTag } from 'root/src/shared/constants/pageData'
 
-export const googleAnalytics = ReactGA
 
 export default () => {
 	ReactGA.initialize(googleTag)
 	ReactGA.pageview(window.location.pathname + window.location.search)
 }
+
+export const googleAnalytics = ReactGA
