f872b65dedcb71e9f6dd5d3e9c63fece136ccf4b
rodmp <rodomiranda2020@gmail.com>
2019-04-04 17:28:10 +0200

    added google analytics

--- src/client/web/app.js
+++ src/client/web/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-
 // css
 import 'react-material-layout/dist/react-material-class-layout.min.css'
 import 'root/src/client/web/app.css'
@@ -9,6 +8,12 @@ import 'root/src/client/web/app.css'
 import RouteRender from 'root/src/client/web/base/RouteRender'
 
 import webStore from 'root/src/client/logic/web'
+// analytics
+import ReactGA from 'react-ga'
+import { googleTag } from 'root/src/shared/constants/pageData'
+
+ReactGA.initialize(googleTag)
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 render(
 	<Provider store={webStore}>
--- src/shared/constants/pageData.js
+++ src/shared/constants/pageData.js
@@ -1,1 +1,2 @@
 export const EMAIL = 'support@DareDrop.com'
+export const googleTag = 'UA-135973404-1'
