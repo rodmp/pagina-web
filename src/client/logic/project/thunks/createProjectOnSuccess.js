d795fdee1f5b25e67052f85ea67f502947e4060f
rodmp <rodomiranda2020@gmail.com>
2019-04-04 22:17:26 +0200

    set up base

--- src/client/logic/project/thunks/createProjectOnSuccess.js
+++ src/client/logic/project/thunks/createProjectOnSuccess.js
@@ -4,6 +4,37 @@ import {
 	DARE_CREATE_SUCCESS_ROUTE_ID,
 } from 'root/src/shared/descriptions/routes/routeIds'
 
-export default () => dispatch => dispatch(
-	pushRoute(DARE_CREATE_SUCCESS_ROUTE_ID),
-)
+import { googleAnalytics } from 'root/src/client/logic/analytics/thunks/googleAnalytics'
+
+// import ReactGA from 'react-ga'
+// import { googleTag } from 'root/src/shared/constants/pageData'
+
+// ReactGA.initialize(googleTag)
+// ReactGA.pageview(window.location.pathname + window.location.search)
+
+export default ({ body }) => async (dispatch) => {
+	const ecommerceData = {
+		id: '1234',
+		name: 'Fluffy Pink Bunnies',
+		sku: 'DD23444',
+		category: 'Party Toys',
+		price: '11.99',
+		quantity: '1',
+	}
+	const transactionData = {
+		id: '1234', // Transaction ID. Required.
+		affiliation: 'Acme Clothing', // Affiliation or store name.
+		revenue: '11.99', // Grand Total.
+		shipping: '5', // Shipping.
+		tax: '1.29', // Tax.
+	}
+
+	googleAnalytics.plugin.require('ecommerce')
+	googleAnalytics.plugin.execute('ecommerce', 'addTransaction', transactionData)
+	googleAnalytics.plugin.execute('ecommerce', 'addItem', ecommerceData)
+	googleAnalytics.plugin.execute('ecommerce', 'send')
+	googleAnalytics.plugin.execute('ecommerce', 'clear')
+	return dispatch(
+		pushRoute(DARE_CREATE_SUCCESS_ROUTE_ID),
+	)
+}
