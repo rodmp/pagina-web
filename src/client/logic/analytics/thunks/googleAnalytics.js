acbd811ea9bd85898f7caf5db35ae0efdc80ec7b
rodmp <rodomiranda2020@gmail.com>
2019-04-06 21:28:11 +0200

    finished ecommerce functionality

--- src/client/logic/analytics/thunks/googleAnalytics.js
+++ src/client/logic/analytics/thunks/googleAnalytics.js
@@ -1,14 +1,13 @@
 import ReactGA from 'react-ga'
 import { developmentGoogleTag, productionGoogleTag } from 'root/src/shared/constants/pageData'
 
- 
 export default () => {
 	if (window.location.host === 'daredrop.com') {
 		ReactGA.initialize(productionGoogleTag)
 	} else {
 		ReactGA.initialize(developmentGoogleTag)
 	}
-
+	ReactGA.plugin.require('ecommerce', { debug: true })
 	ReactGA.pageview(window.location.pathname + window.location.search)
 }
 
