4a7311aa8dfabb6444811549214e90cf4c566f0a
CoolFullstackDev <topace1029@hotmail.com>
2019-04-07 23:16:53 +0300

    addToFavorites functionality done

--- src/client/logic/project/handlers/goToAddFavoritesHandler.js
+++ src/client/logic/project/handlers/goToAddFavoritesHandler.js
@@ -1,7 +1,4 @@
-import {
-	PLEDGE_PROJECT_ROUTE_ID,
-} from 'root/src/shared/descriptions/routes/routeIds'
 
-export default (recordId, pushRoute) => () => (
-	pushRoute(PLEDGE_PROJECT_ROUTE_ID, { recordId })
+export default (addToFavorites) => () => (
+	addToFavorites()
 )
