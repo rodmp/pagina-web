e4fcfab444b7610dbb53c80fe9ba15ca57a9320f
rodmp <rodomiranda2020@gmail.com>
2019-04-12 18:25:59 +0200

    fixed bug

--- src/server/api/actions/index.js
+++ src/server/api/actions/index.js
@@ -3,8 +3,7 @@ import {
 	GET_OAUTH_TOKENS, ADD_OAUTH_TOKEN, CLEAR_PARTIAL_FORM_KEYS,
 	AUDIT_PROJECT, GET_ACTIVE_PROJECTS, GET_PENDING_PROJECTS,
 	GET_PAYMENT_METHODS, ADD_PAYMENT_METHOD, DELETE_PAYMENT_METHOD, UPDATE_PROJECT,
-	SAVE_PARTIAL_DARE_FORM, SET_DEFAULT_PAYMENT_METHOD, UPDATE_PROJECT,
-	CLEAR_PARTIAL_FORM_KEYS,
+	SAVE_PARTIAL_DARE_FORM, SET_DEFAULT_PAYMENT_METHOD,
 } from 'root/src/shared/descriptions/endpoints/endpointIds'
 
 import getProject from 'root/src/server/api/actions/getProject'
@@ -22,8 +21,6 @@ import getPaymentMethods from 'root/src/server/api/actions/getPaymentMethods'
 import addPaymentMethod from 'root/src/server/api/actions/addPaymentMethod'
 import deletePaymentMethod from 'root/src/server/api/actions/deletePaymentMethod'
 import setDafaultPaymentMethod from 'root/src/server/api/actions/setDefaultPaymentMethod'
-import savePartialDareForm from 'root/src/server/api/actions/savePartialDareForm'
-import clearPartialFormKeys from 'root/src/server/api/actions/clearPartialFormKeys'
 import updateProject from 'root/src/server/api/actions/updateProject'
 
 export default {
