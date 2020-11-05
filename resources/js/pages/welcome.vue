5e7dfc0c81c74559d39e2a8404c033da7ed84a43
Rodolfo <rodomiranda2020@gmail.com>
2020-11-05 12:31:59 -0800

    Create new project with laravel 8.4.0

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Welcome Page</div>
+                    <div class="card-body">
+                        This is Welcome Page
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Welcome Component mounted.");
+    }
+};
+</script>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<main-page></main-page>
+@endsection
