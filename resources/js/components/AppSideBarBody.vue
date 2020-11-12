678391ce59be543950257405658b9a9f5a017f73
Rodolfo <rodomiranda2020@gmail.com>
2020-11-12 12:48:50 -0800

    Add sidebar body and footer component

---
+++ resources/js/components/AppSideBarBody.vue
@@ -0,0 +1,18 @@
+<template>
+    <section
+        v-if="isCollapsedFooter"
+        class="cp-nav-container flex-grow-1"
+    ></section>
+</template>
+<script>
+export default {
+    mounted() {
+        console.log("AppSideBarBody Component mounted.");
+    },
+    props: {
+        isCollapsedSideBar: Boolean,
+        isCollapsedFooter: Boolean
+    }
+};
+</script>
+<style scoped></style>
